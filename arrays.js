312166b24c7586ca61f7c5cc44610824d510ae1d
kim tran <icycleitaly@gmail.com>
2018-03-20 00:56:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,4 +25,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array;
 }
 
-function removeElementFromBeginningOfArray(array)
+function removeElementFromBeginningOfArray(array){
+  return array.slice(0);
+}
+
